1dc1c6cf1224d3be73abd79f3387a3a7a9a26d7c
dhyey <dbshah@ibm.com>
2017-05-03 11:18:01 -0400

    more changes to the start script

--- peer-image/start.sh
+++ peer-image/start.sh
@@ -28,20 +28,31 @@ else
 	echo "CA Password is ***** :P"
 fi
 
+if [ -z ${TLS_HSBN_CERT} ]; then
+	echo "Please set the TLS_HSBN_CERT"
+	exit 5
+else
+	echo "TLS HSBN cert is at ${TLS_HSBN_CERT}"
+fi
+
 export FABRIC_CA_CLIENT_HOME=${CORE_PEER_MSPCONFIGPATH}/..
-export CORE_PEER_MSPCONFIGPATH=${CORE_PEER_MSPCONFIGPATH}/
 
 echo "Getting the certs from CA"
 if [ "${CORE_PEER_TLS_ENABLED}" == "true" ]; then
-	echo "fabric-ca-client enroll -u https://${CA_USER}:CA_PASSWORD@${CA_HOST}:${CA_PORT} --tls.enabled --tls.certfiles ${CORE_PEER_TLS_ROOTCERT_FILE}"
-	fabric-ca-client enroll -u https://${CA_USER}:${CA_PASSWORD}@${CA_HOST}:${CA_PORT} --tls.enabled --tls.certfiles ${CORE_PEER_TLS_ROOTCERT_FILE}
+	echo "fabric-ca-client enroll -u https://${CA_USER}:CA_PASSWORD@${CA_HOST}:${CA_PORT} --tls.enabled --tls.certfiles ${TLS_HSBN_CERT}"
+	fabric-ca-client enroll -u https://${CA_USER}:${CA_PASSWORD}@${CA_HOST}:${CA_PORT} --tls.enabled --tls.certfiles ${TLS_HSBN_CERT}
 else
 	echo "fabric-ca-client enroll -u http://${CA_USER}:CA_PASSWORD@${CA_HOST}:${CA_PORT}"
 	fabric-ca-client enroll -u http://${CA_USER}:${CA_PASSWORD}@${CA_HOST}:${CA_PORT}
 fi
 
-cp -r ${CORE_PEER_MSPCONFIGPATH}/cacerts/ ${CORE_PEER_MSPCONFIGPATH}/admincerts/
-#mkdir -p ${PEER_CFG_PATH}/msp/sampleconfig
+set +e
+mv ${CORE_PEER_MSPCONFIGPATH}/cacerts/*.pem ${CORE_PEER_MSPCONFIGPATH}/cacerts/cacert.pem
+mv ${CORE_PEER_MSPCONFIGPATH}/cacerts/.pem ${CORE_PEER_MSPCONFIGPATH}/cacerts/cacert.pem
+set -e
+mkdir -p ${CORE_PEER_MSPCONFIGPATH}/admincerts
+cp ${CORE_PEER_MSPCONFIGPATH}/cacerts/* ${CORE_PEER_MSPCONFIGPATH}/admincerts/.
+mkdir -p ${PEER_CFG_PATH}/msp/sampleconfig
 
 # start the peer executable
 peer node start --peer-defaultchain=false
